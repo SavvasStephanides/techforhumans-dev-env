edc0a25c432be9c470ab62bf283eb2eaa1e08119
Savvas <savvascyp@hotmail.com>
2023-07-23 16:40:20 +0100

    Script fixes

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ git clone https://github.com/devforhumans/web
 
 cd posts
 docker run -it --rm -v $PWD:/posts -w /posts node:18-alpine3.17 yarn
-docker run -it --rm -v $PWD:/posts -w /posts node:18-alpine3.17 yarn start
+docker run -it --rm -v $PWD:/posts -w /posts node:18-alpine3.17 yarn build
 
 cd ../web
-docker run -it --rm -v $PWD:/posts -w /posts node:18-alpine3.17 yarn
+docker run -it --rm -v $PWD:/web -w /web node:20-alpine3.17 npm i
